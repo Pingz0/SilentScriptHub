2f8efbc325338f3f88f8688ea68437c016c3f897
Pingz0 <jkdpsc88@gmail.com>
2025-07-30 15:11:02 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ local Window = Rayfield:CreateWindow({
     ConfigurationSaving = {
         Enabled = false,
     },
-    
+    Theme = "Amethyst"
     KeySystem = true,
     KeySettings = {
         Title = "Silent Access",
